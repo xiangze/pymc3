de9f7dfaea62e1a73d012b3396075ff8b52eb34f
Chris Fonnesbeck <chris.fonnesbeck@vanderbilt.edu>
2016-05-27 11:13:47 -0500

    Partial fix to invlogit

--- pymc3/math.py
+++ pymc3/math.py
@@ -7,14 +7,17 @@ from theano.tensor import maximum, minimum, sgn, ceil, floor
 from theano.tensor.nlinalg import det, matrix_inverse, extract_diag, matrix_dot, trace
 from theano.tensor.nnet import sigmoid
 import theano
-from theano import ifelse
 import theano.tensor as tt
 import sys
 
 def invlogit(x):
     x_max = -tt.log(sys.float_info.epsilon)
-    x = ifelse(tt.gt(x, x_max), 1.0, ifelse(tt.lt(x, 1-x_max), 0.0, x))
-    return 1/(1 + tt.exp(-x))
+    return tt.switch(tt.gt(x, x_max), 
+            1.0, 
+            tt.switch(tt.lt(x, 1-x_max), 
+                0.0, 
+                1/(1 + tt.exp(-x))))
+     
     
 def logit(p):
     return tt.log(p/(1-p))
